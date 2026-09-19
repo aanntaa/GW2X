8798bbd8361be431e882cb238bf85b2f0bcbebd6
aanntaa <aanntaa@gmail.com>
2026-09-19 13:48:38 +0700

    Update USER.py

---
+++ USER.py
@@ -0,0 +1,353 @@
+
+donator = """
+==|0002710M|== \n 
+LEGENDARY COMMANDER : \n
+-K W, -I P, -H W, D M M \n
+\n\n
+ASCENDED COMMANDER : \n
+RZ, NMS, NH, MDB, JS, JA, FC, RB, VR, MDG, RH, PLC, FOX, SN, OYI, 
+WG, ES, MQ, RD, BK, NK, TT, AC, RP, IV, JX, RS, AKM, FRT, SP, VL, 
+FQ, LX, DZ, ER, CJ, LC, NJ, MZ, TH, AK, TD, LM, NE, RDN, BC, JM, 
+CC, HG, DW , DU, SQ, TG, SS, MT, FF, RH, TT, CK, BT, XR, SR, EZP,
+AZ, CB, DY, KM, ACT, PM, AB, SX, AB, RP, DN, NN, AS, AF, VM, TS, CT,
+KG, TX, DP, NEO, KL, MT, KN, DMR, TZ, HB, VC, MK, NN, YC, MC, IS,
+SS, XK, AK, AT, CW, SL, DD, AS, AV, GH, KK, FK, GM, MM, MF, RG,
+RS, CV, XC, GR, OD, SK, SC, SJ, NC, HG, VK, TY, BB, DV, AU, GB, HC,
+UL, RZ, AP, DW, GC, HM, SV, CG, CP, RW, VC, UM, PC, XR, NT, MC,
+KK, HF, MF, JN, PP, HZ, KJ, RN, GV, CT, CM, HJ, GK, DM, FL, MG, RB,
+BL, SN, DW, MM, GG, IC, RH, KW, SG, VR, AX, TX, MT, SW, RC, TK, AR,
+YK, BS, JD, CR, VL, RC, RO, SJ, HA, WT, LO, EV, SO, HC, QR, KJ, TR,
+FH, JD, FP, AJ, TR, MM, BN, EU, MZ, TR, BB, AF, RD, MD, KT, AR, OG,
+SF, TB, IZ, CB, IC, SP, RN, ZL, IC, WK, XR, SH, TV, MN, GC, SD, XE \n
+============================\n
+
+GW2X Exist and maintained since April 2023\nconsider donating to keep this project alive\nNO SUBSCRIBTION REQUIRED\n
+thanks for your support !!!
+"""
+#=============
+#=============
+#=============
+
+uuid_saved = ["e666a5b6610e","9fa1f56921b4","c5d83ec5fc2b","5584f72d04f7","bbe4dd66e5b7","bd6c8317593e","7f2667bc6dfc","cd8a7233c515","28d007b687d5","6f45e22ec708",
+             "88bf59952163","7bedc41ad5b6","1303cfb17242","61725d1efcd0","4879801bc9b0","70350bfd0594","410f7e68ce7c","718ceed60a68","26776c09655e","6839045e57b0",
+             "eb8bd8caffb6","e195c193612b","3deeab301246","ef8a3d3a8e90","33ff90fbb004","14dd987aade2","8e07e13880d9","a7d217159b3e","ce63e03d4633","9dfdb9eb4b2b",
+             "adbda1cb7b25","da91f16e0273","544cacde2182","0fe30d9aefb7","2f407a8c87fc","38fa8ee4da23","a9dccb506c93","3f3e4bf4774e","73596a0d1b4b","407deca13712",
+             "1dd34f147241","688b9ce0efc9","c20af37ca609","0aa927848ddd","e466585c14dc","8cb166d967ba","44ccb763a00e","19235935724e","cac006aac84f","4e1f99da8f0e",
+             "0f74bb9974c3","57e691655447","7fc15567bdc7","9d6ba27c6635","972857b6201b","3d62670a8bed","6856f2dfdcc8","e004897274f4","02212ecdac9e","900325a0ab1f",
+             "2a185766f3bf","d8df82530049","390ced62abc5","4b08dcbfe5bb","a71b5827f42a","51507ffd6521","dca64f35a2a7","b53fe1c7db73","ae03971d5951","e0b9ceb8485f",
+             "79ff43aa4fa8","d5935edeac66","5c9f5c32c601","c58ae07ba84c","74495f9e6b24","fc33b84d26a9","b683298fe41d","02ffe4a90862","6a7c5f6cd0f6","a35329a359dc",
+             "282208cc20f8","d5f8596da991","f9414e357b64","5b370cab1e0a","b65512c57141","bc6d7b39acf8","d91846b481b6","575d468d903b","a1d2f539858a","d895b9f2c212",
+             "8b7ccd302fc3","7fd9ce29b273","6c054fd2df08","b6cdedb0c3e5","97f6628d0558","bb7db7542533","1969dee4581c","47aa491dc988","620a56a09159","2fb67367b2f3",
+             "c3db8b39f336","bc7c843c21ec","020ee934e806","3df932ca2794","d39882a5d11b","103b75ddae0d","5011e1070865","157519e0ec0d","c01a2c09de71","099c9631b71d",
+             "67f0e3df0297","6e2877292a2c","253e0730a52e","9a9309d6141c","d124ec0d35f4","f7eb05aa7621","c86b80dc95f0","05380c3e4512","676544bb325e","34d933a5ee67",
+             "ead954d2a5ce","4739e0169158","0286e33049a8","bbe2001e9659","701b067b9dd7","19f8d8d8a18f","aa27a57b415a","f28e5c15146a","9fd732c35ad3","a754914e029e",
+             "5d47ee16c526","6225354ce65f","a967c2af7bb6","31cfe0807231","de591bc31c94","8be26879a40d","9f5969b96917","f38c76e8c783","8d41d5a29c41","83da83063aad",
+             "dad694a70742","854d2b1fe1e8","4014f11a3031","86d0fe2b5751","f47323e85171","e0620500eb2c","f86970295e19","9a4b4ccd9a86","040cf1312478","3f4533773c51",
+             "8dd2e0be4516","19c6e8b88e7e","346a7c873716","7881c084bdf9","e7454b35a57a","ed9f842dd9af","963b49b26d3d","280e48064854","62bc6826ebaf","92f494b5a939",
+             "de50d40e5e63","fc2c7ebc512a","8cadb5116a5f","f3da2ff2a3fa","5175c0d7d08c","f1df154f021f","c55bc3a6e78b","484899890ac3","8f3e1fbdebfa","5bdf083e645e",
+             "14d2f4cedfa7","5a2ccabc5781","f132a87d201f","330a591f58d1","9ba82abc0996","829c3c3f04f0","59b94d9ec5c0","c5cf494703d9","b3208fd1ed5e","c50ea8e865f4",
+             "775c2624472a","d640589847b7","282846dd5496","88051f2cc3d7","bad63eff9b32","8f85ac8ec363","247f603e1846","999fb0e0c370","dc18c079e791","5cc93e04139f",
+             "dce2671d7595","243b61b67574","d4d2b6721bb1","5ff9c2a596c1","3e31cc5d03ea","e8057f29a7cc","3120fc1e12f0","58d6ecd0b2ed","496dad68ca1e","76d03a8944f5",
+             "97bcf76d245a","a341444bfbd8","458b87df3371","4d3d5f6a5df1","3fe19811573d","26a0b4bdc71f","0926efcdf32a"]
+
+
+
+
+''' 
+3fe19811573d = matheus_camargos XxCAPIVARAxX
+4d3d5f6a5df1 = paypal dude
+458b87df3371 = Xerandaal wilton7712
+97bcf76d245a = seidell
+76d03a8944f5 = grinmircoop
+3120fc1e12f0 = archer_kush
+3e31cc5d03ea = sidolab
+243b61b67574 = stelacia
+dce2671d7595 = midnighx
+247f603e1846 = toverla
+8f85ac8ec363 = runningslowly sohey
+bad63eff9b32 = xerev
+88051f2cc3d7 = arielau
+282846dd5496 = Zinc wakimme
+d640589847b7 = iakovillas JACKYS
+5ff9c2a596c1 = iakovillas JACKYS 2
+c50ea8e865f4 = Hashi Zilent
+b3208fd1ed5e = rain0500
+59b94d9ec5c0 = sephael
+9ba82abc0996 = icosite
+330a591f58d1 = coffeeblack
+14d2f4cedfa7 = izzy
+5bdf083e645e = aHamSandwich terdboy
+8f3e1fbdebfa = srfuton
+c55bc3a6e78b = ombrellogiallo
+f1df154f021f = aanntaa Rubster
+5175c0d7d08c = ktmrc8r
+f3da2ff2a3fa = matsadura am Zidane
+8cadb5116a5f = rodrigoduarte2788
+de50d40e5e63 = trippy.jonas
+62bc6826ebaf = bloodhound1962
+7881c084bdf9 = Taru4759
+f47323e85171 = megazord_64569
+4014f11a3031 = euan_27
+854d2b1fe1e8 = Benny justamain
+f38c76e8c783 = mamang tracks8912
+8be26879a40d = tiranus84
+de591bc31c94 = ajlxxgb
+31cfe0807231 = felipemg1991
+a967c2af7bb6 = jose_dieguez
+a754914e029e = faulerhobbit_41913
+9fd732c35ad3 = terifiqa
+f28e5c15146a = kejbord
+19f8d8d8a18f = qiro
+0286e33049a8 = hyckz
+4739e0169158 = soe
+ead954d2a5ce = evelyn
+f7eb05aa7621 = lolbugs None
+9a9309d6141c = Blame It on Top whatthefuckyoumean
+253e0730a52e = hayven gruenerkodo
+6e2877292a2c = sojiken wendys
+67f0e3df0297 = rook
+099c9631b71d = rchompz
+c01a2c09de71 = volos
+157519e0ec0d = coussiraty
+103b75ddae0d = JordGPT uruk.guy
+3df932ca2794 = bbshark23
+020ee934e806 = yukiochan
+c3db8b39f336 = anyrouz
+2fb67367b2f3 = turkushan
+1969dee4581c = richniomanes
+bb7db7542533 = sweps4life
+d895b9f2c212 = leo pupin
+575d468d903b = yznyu
+d91846b481b6 = 7amood771
+bc6d7b39acf8 = matthw7010
+5b370cab1e0a = testex
+d5f8596da991 = lanceiro1756
+282208cc20f8 = songokeush
+a35329a359dc = varleria
+6a7c5f6cd0f6 = smolghost
+02ffe4a90862 = keywiz
+b683298fe41d = rhoid899 1
+b65512c57141 = rhoid899 2
+79ff43aa4fa8 = invadertippsy
+b53fe1c7db73 = Gorgon
+dca64f35a2a7 = modestlymoustachioed
+4b08dcbfe5bb = Catalyst
+390ced62abc5 = Chelliebellie
+e004897274f4 = iidwestii
+6856f2dfdcc8 = snow6255
+3d62670a8bed = not.richard
+19235935724e = beelsan
+44ccb763a00e = robbyear 
+0aa927848ddd = migetno1
+1dd34f147241 = Fallen
+407deca13712 = snoobie
+38fa8ee4da23 = serverleaderhunk Retired gamer
+da91f16e0273 = hirobutton
+adbda1cb7b25 = elpumita
+410f7e68ce7c  = Maerath f7ss27x5b4xbejoi mig07zsrngsktn Fenlorn  dol8v4zodizi38
+c58ae07ba84c = davggmane
+c5d83ec5fc2b = gokiburi8296
+d5d1d4a10d = hjjr78
+9fa1f56921b4 = clementming -
+3deeab301246 = CraTzy
+9dfdb9eb4b2b = GNGR_VKNG
+c86b80dc95f0 = Runnae (saekoslab)
+5d47ee16c526 = Kinjake
+999fb0e0c370 = Hizzy
+5584f72d04f7 = Porpor
+14dd987aade2 = j4n
+6f45e22ec708 = Mheleficent
+58384f5f26 = Hamad771 friends
+6839045e57b0 = Kookoochoo 8loise
+0926efcdf32a = mc35879
+51507ffd6521 = nesstor1099
+bc7c843c21ec = Xryeth khal4843
+83da83063aad = Porchy
+b6cdedb0c3e5 = universeelementor_63535
+a4b3afed94 = vector_4444 wise old man
+19c6e8b88e7e = reisw
+0fe30d9aefb7 = christmas_pud
+1ede003201 = chargovinda
+5011e1070865 = Silviohc
+a7d217159b3e = hamad771
+e0b9ceb8485f = gr4cko
+d4d2b6721bb1 = nelson (doowen)
+515aee78c8 = appledinner
+f132a87d201f = razpdr
+5cc93e04139f = morganj23
+d5935edeac66 = hoilch
+dad694a70742 = bigbacg
+cd591db0ae = auburnsprings
+70350bfd0594 = david
+5a2ccabc5781 = beanboifresh
+0f74bb9974c3 = tyrantt0_36151
+8b681b9078 = akulf123 riend
+e53d258881 = vekqurin
+97f6628d0558 = hudsightgamer dexire
+701b067b9dd7 = nicocg
+93582e4eae = ssaljoone
+e0a8f8edaa = scatterfrog
+5c9f5c32c601 = skullylol
+3f3e4bf4774e = oddphantomhayden
+bd9f2ddb16 = Guerini
+df0bb99b35 = xChasseur
+e466585c14dc = Corvo (liarliarpunsonfyre)
+5af3d5acf8 = RedSkyMessenger
+5fef652218 = Adam AC
+d28f134389 = temporary trial (removed)
+28d007b687d5 = rdenog
+ce63e03d4633 = MechFan ihateusingdisc yepdontcar
+0c28dc0eb4 = MMoaddict
+c0d43a2075 = Gumagan
+64e0c9c9d9 = Father Kofo
+3f4533773c51 = Kinnikuman
+b8519516f4 = Greenhaus
+596851a385 = Avadon
+6225354ce65f = Assassin
+7fd9ce29b273 = daddio9646
+900325a0ab1f = shlomotion
+1f9fa1ada9 = ChillyWilly
+5d7ceb6425 = ChillyWilly2
+34d933a5ee67 = Alte
+8cb166d967ba = Arke
+ef8a3d3a8e90 = xk xkxina gothspookyangel
+f7ee09dae4 = systemsoldier
+8dcc3f17ce = Isso
+5d4adcbfaf = Mercy
+496dad68ca1e = jasonyc YC
+bd6c8317593e = Neorinakota
+f86970295e19 = Mohki
+eb020857aa = Vincef
+9f5969b96917 = hibrick
+93582e4eae = tanztante
+0a1e3b4a9c = kanei
+f334e9a3ac = Dr Murphy Richard
+7bedc41ad5b6 = Matt77 2 (World Explorer)
+e640683c72 = Matt77 (World Explorer)
+346a7c873716 = Kil
+18976cdcf1 = neo14a
+a341444bfbd8 = Doople
+2d8c04efd3 = Doople 2
+7e01fb6c55 = TUX1337
+d8df82530049 = Knallgurke 2 laptop
+a71b5827f42a = Knallgurke pc
+fc5966ec43 = Critias
+2f407a8c87fc = TSAO
+0d0f19b7a1 = TSAO 2
+963b49b26d3d = Afflictedfury
+92f494b5a939 = Afflictedfury 2
+484899890ac3 = Astro uzakgezegen
+97f88c63d7 = Nossninja
+cd8a7233c515 = Don - oranjelly
+88bf59952163 = Røpke
+676544bb325e = ASTRO 80Y
+9d6ba27c6635 = Shaxen bjokes
+ccda0f3c76 = Articvb
+1433e3fbf0 = Articvb 2
+eb8bd8caffb6 = unloadammo Private Marx
+86d0fe2b5751 = Alcatraz 
+a9dccb506c93 = Kilmanagh
+c155f9c7d4 = dokyun
+d2e264842a = colebear
+7457621e28 = AzRa
+33ff90fbb004 = EdZoPlay
+4879801bc9b0 = TheTwork Sveik584
+61725d1efcd0 = TheTwork
+c5cf494703d9 = Steryan
+fec12d3dfe = xtraRice
+58d6ecd0b2ed = Bolt
+e8057f29a7cc = Cekko
+57e691655447 = RenjaHB
+40dc55a18b = FamousFarrow
+775c2624472a = Morticus
+4b49224938 = Sam Silva
+d124ec0d35f4 = Tiger
+1af7f3316f = sasquash
+613007de10 = Daunt
+3778c59307 = LordVader removed
+6a5db5599a = Dutchwise
+fee0c0b244 = HiGW
+bbe2001e9659 = Cekcenk
+280e48064854 = Cekcenk new vm 
+cad32eb6ab =  jiimmy
+0f15f4702e = Biscotti
+829c3c3f04f0 = RdN
+c178ab4972 = No62NoExit 2 laptop
+f151b202dc = No62NoExit
+91549412a3 = No62NoExit 3 (off, waiting new pc)
+3afeaf99ff = No62NoExit 4 
+05380c3e4512 = Lucas--MileFantazy
+e7454b35a57a = Lucas--MileFantazy 2
+ed9f842dd9af = Lucas--MileFantazy 3
+fc2c7ebc512a = Lucas--MileFantazy 4
+26a0b4bdc71f = Taldor
+171ad776bc = akull123456
+1ff58644c8 = TheHacker old off
+2a185766f3bf = TheHacker
+4e1f99da8f0e = Mizunie
+1bcd371a24 = Mizunie 2
+0d5192aaa8 = Mizunie 3
+9aeaaceef9 = Mizunie 4
+8d41d5a29c41 = NJ
+1303cfb17242 = khicon
+d461de7177 = Level 1 Crook
+42339b90f9 = Level 1 Crook 2
+26c741dda2 = Level 1 Crook 3
+b427b63072 = Level 1 Crook 4
+8b07915f7e = Level 1 Crook 5
+96029128c9 = carrjam
+620a56a09159 = Error
+dc18c079e791 = Dumnezeus
+b679c63915 = Luxz
+040cf1312478 = Freq
+e53d258881 = Villain
+75a4159f86 = SquishyPoo1870
+26776c09655e = FRTZ
+718ceed60a68 = FRTZ 2 vm
+01035bf5b7 = Akemi
+6c054fd2df08 = Robsie
+8dd2e0be4516 = J4XXXOR
+e195c193612b = ivicask
+f9414e357b64 = ivicask 2
+aa27a57b415a = ivicask (main a)
+da698b0e4c = RarePepe
+47aa491dc988 = Aiden Cross 
+3496ec8c63 = tiredtbh
+e2823149be = Naksu
+01035bf5b7 = Beutelkramer
+e84e400c61 = ridicul0us
+2bdc737541 = Morqitta 3
+68085ef0f0 = Morqitta 2
+9e87fbea81 = Morqitta new
+33bfcbbadc = EddieSmurphy
+d35d2ec041 = wongo
+8e07e13880d9 = Truckingsteve#007
+df33e92ff7 =  OD YR INK off
+73596a0d1b4b = Kaisen OD YR INK (hay_low)
+5eb7799781 = Syrix removed
+e0620500eb2c = Sinic
+cac006aac84f = フォックスマン shermilion
+9a4b4ccd9a86 = polychromatik
+ae03971d5951 = Reeeehaww joshmcfarland
+0f407ed34e = Mindaugas OFF
+7fc15567bdc7 = Mindaugas New
+5debc04d27 = Hawaiian
+02212ecdac9e = Varek
+c20af37ca609 = falco
+6fe74d85f3 = Jaxx Armstrong
+ecc010a66e = don't mind me 3
+b275bc6551 = don't mind me 2
+53b625f3d9 = don't mind me
+74495f9e6b24 = morbiddarkblood
+cf80d1275b = NtajHmoob
+544cacde2182 = nemesis
+688b9ce0efc9 = Nemesiss 2 laptop
+3678bde5e3 = intel_power
+2fda4ec1b0 = kowa ( GIVE ACCOUNT TO Robbes )
+972857b6201b = Robbes
+d39882a5d11b = rietzee
+e666a5b6610e = sabay
+
+'''
+#=============
+#==============
